e21d27de3d95237d2742dec2e9dbcc79f3954372
sergey12malyshev <59499846+sergey12malyshev@users.noreply.github.com>
2024-06-13 22:43:30 +0500

    Добавляю скрипт обновления по uart

---
+++ updateFirmware.cmd
@@ -0,0 +1,6 @@
+rem Upload the program file to the 0x08000000 flash memory address use usb-uart adapter
+
+D:\CubeProgrammer\bin\STM32_Programmer_CLI.exe -c port=/COM4 br=115200 -w build\RF_HACK.hex --go
+
+
+pause
