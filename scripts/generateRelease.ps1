5dade3ddb16c2051fb0f3e24e446e201e2da0f21
sergey12malyshev <59499846+sergey12malyshev@users.noreply.github.com>
2025-09-20 19:42:26 +0500

    update release script

--- scripts/generateRelease.ps1
+++ scripts/generateRelease.ps1
@@ -1,21 +1,29 @@
 # Getting the current Git tag
 $tag = git describe --tags --abbrev=0
 
+$git_hash = git log -1 --format="%h"
+
 $filename = "RF_HACK"
 
 # Checking if the Git tag is installed
 if ($tag -ne "") {
+  
+  $timestamp = Get-Date -Format "dd.MM.yyyy"
   # Creating an archive name using a tag
-  $archiveName = "$filename$tag.zip"
+  $archiveName = "$filename" + "-v" + "$tag" + "-d" + "$timestamp.zip"
   
 
   # The path to the file to archive
   $filePathHex = "build/$filename.hex"
   $filePathBin = "build/$filename.bin"
+  $fileReadme =  "build/Readme.txt"
 
+  Set-Content -Path $fileReadme -Value "Firmware $filename version: $tag data build: $timestamp hash commit: $git_hash."
+  
   # Archiving the file to a ZIP archive
   Compress-Archive -Update -Path $filePathHex -DestinationPath $archiveName
   Compress-Archive -Update -Path $filePathBin -DestinationPath $archiveName
+  Compress-Archive -Update -Path $fileReadme -DestinationPath $archiveName
 
   # The output of the successful archiving message
   Write-Host "The file was successfully archived $archiveName"
